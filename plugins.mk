c2a19cab2545c2eeb213b2761db4c9a9d261c909
sadko4u <sadko4u@gmail.com>
2022-01-16 01:43:34 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-room-builder.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_ROOM_BUILDER
-ARTIFACT_NAME               = lsp-plugins-room-builder
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Room Builder Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.2-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_ROOM_BUILDER
+PLUGIN_PACKAGE_NAME     = lsp-plugins
