86f718fbc5f7d61605f8488f98362d3b20287ff1
sadko4u <sadko4u@gmail.com>
2022-05-27 22:57:51 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,7 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-room-builder
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Room Builder Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.4-devel
+ARTIFACT_VERSION            = 1.0.4
 
 
 
