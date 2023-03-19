c64a545ce7ef50f8d634a7cfb9929bb3191118ea
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-19 15:24:10 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -390,6 +390,7 @@ $(LSP_PLUGINS_ROOM_BUILDER_BIN)/main/ui/room_builder.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPlayListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ProxyPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
