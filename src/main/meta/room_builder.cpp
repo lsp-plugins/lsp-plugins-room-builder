347529aaa5e9a8513412adc64ef40c1e7131ada4
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-04 23:45:43 +0300

    Using new mechanism for registering plugin metadata

--- src/main/meta/room_builder.cpp
+++ src/main/meta/room_builder.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugins-room-builder
  * Created on: 3 авг. 2021 г.
@@ -20,6 +20,7 @@
  */
 
 #include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <lsp-plug.in/plug-fw/meta/registry.h>
 #include <lsp-plug.in/shared/meta/developers.h>
 #include <lsp-plug.in/common/status.h>
 #include <private/meta/room_builder.h>
@@ -511,6 +512,7 @@ namespace lsp
             mono_to_stereo_plugin_port_groups,
             &room_builder_bundle
         };
+        LSP_REGISTER_METADATA(room_builder_mono);
 
         const meta::plugin_t  room_builder_stereo =
         {
@@ -541,5 +543,7 @@ namespace lsp
             stereo_plugin_port_groups,
             &room_builder_bundle
         };
+        LSP_REGISTER_METADATA(room_builder_stereo);
+
     } /* namespace meta */
 } /* namespace lsp */
